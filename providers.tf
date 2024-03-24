6e37874418f98a6e8d18704dd499cacab11ef8d9
Abhinandan Joshi <abhinandanjoshi99@gmail.com>
2024-03-24 11:28:11 +0530

    chnages to github actions

--- providers.tf
+++ providers.tf
@@ -9,8 +9,36 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+      resource_group_name  = "rg-github-actions"
+      storage_account_name = "sagithubactions"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
+}
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
 }

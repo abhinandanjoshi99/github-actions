5491543b5d3f627b0de770b161d5b55ab2975419
abhinandanjoshi99 <77507393+abhinandanjoshi99@users.noreply.github.com>
2024-03-24 11:36:58 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -21,24 +21,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-
-
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "eastus"
-}
